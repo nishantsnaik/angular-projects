9ef0a9cbadd962c2d305afddab9987ed14742152
Nishant Naik <nishant.naik@davita.com>
2019-06-23 19:52:33 -0500

    recipe-book-commits

---
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://smokeystavern.com/wp-content/uploads/2018/03/osemary-grilled-salmon.jpg'),
+    new Recipe('A Test Recipe','This is simply a test','https://smokeystavern.com/wp-content/uploads/2018/03/osemary-grilled-salmon.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
