c863185fbe11983a429027601933180c4e215efb
Nishant Naik <nishantsnaik@gmail.com>
2019-07-14 18:44:33 -0500

    Added service layer to shopping list componenet

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import {FormsModule} from '@angular/forms';
 import {DropdownDirective} from './shared/dropdown.directive';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import {DropdownDirective} from './shared/dropdown.directive';
   imports: [
     BrowserModule, FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,16 +1,42 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected  = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://smokeystavern.com/wp-content/uploads/2018/03/osemary-grilled-salmon.jpg'),
-    new Recipe('2 Test Recipe', 'This is second test', 'https://smokeystavern.com/wp-content/uploads/2018/03/osemary-grilled-salmon.jpg')
+    new Recipe(
+      'Chicken Biryani',
+      'Rice dish loaded with spicy marinated chicken, caramelized onions',
+      'https://norecipes.com/wp-content/uploads/2017/05/chicken-biryani-12.jpg',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Rice', 1),
+        new Ingredient('Onions', 2),
+      ]),
+    new Recipe('Pav Bhaji',
+      'spicy mix vegetable mash & soft buns',
+      'https://hebbarskitchen.com/wp-content/uploads/2016/12/pav-bhaji-recipe-in-cooker-quick-easy-pav-bhaji-recipe-16-769x1024.jpeg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Onions', 3)
+        ])
   ];
+
+  constructor(private slService: ShoppingListService) {
+
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+
   }
 
 }
